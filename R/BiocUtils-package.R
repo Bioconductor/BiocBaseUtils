7c1e43f90e6486d651c691c566fca0758c7363f4
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-04-01 17:28:07 -0400

    add vignette and DESCRIPTION

--- R/BiocUtils-package.R
+++ R/BiocUtils-package.R
@@ -3,9 +3,7 @@
 #' BiocUtils attempts to formalize often written developer functions that
 #' are scattered throughout the Bioconductor ecosystem. Some of these functions
 #' include the ability to replace slots in an object. Other functions work
-#' to create a nice show method output by selecting some observations. This
-#' package also allows developers to check what emails are associated with
-#' what packages.
+#' to create a nice show method output by selecting some observations.
 #'
 #' @aliases NULL
 "_PACKAGE"
